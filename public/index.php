f8ab7d3b4e0d1f9b11dc3516020ba465df275db8
Steve Poskitt <steve@spectrasoftware.co.uk>
2018-04-25 15:44:32 +0100

    Upates to make sure previous fixes included.

--- public/index.php
+++ public/index.php
@@ -7,11 +7,43 @@ $twig = new Twig_Environment($loader, ['debug' => true]);
 
 //Get the episodes from the API
 $client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-$data = json_decode($res->getBody(), true);
+$error = 0 ;
+try{
+	$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
+	$data = json_decode($res->getBody(), true);
 
-//Sort the episodes
-array_multisort(array_keys($data), SORT_ASC, SORT_STRING, $data);
+	//Sort the episodes
+	$data = sort_multi_array( $data, "season","episode");
 
-//Render the template
-echo $twig->render('page.html', ["episodes" => $data]);
+	$seasons = array_unique( array_column( $data, "season" ) );
+
+}
+catch( GuzzleHttp\Exception\ServerException $e ){
+	$error = 1 ;
+}
+
+	//Render the template
+
+echo $twig->render('page.html', ["error"=>$error, "episodes" => $data, "seasons" => $seasons]);
+
+
+function sort_multi_array ($array, $key)
+{
+  $keys = array();
+  for ($i=1;$i<func_num_args();$i++) {
+    $keys[$i-1] = func_get_arg($i);
+  }
+  // create a custom search function to pass to usort
+  $func = function ($a, $b) use ($keys) {
+    for ($i=0;$i<count($keys);$i++) {
+      if ($a[$keys[$i]] != $b[$keys[$i]]) {
+        return ($a[$keys[$i]] < $b[$keys[$i]]) ? -1 : 1;
+      }
+    }
+    return 0;
+  };
+  
+  usort($array, $func);
+  
+  return $array;
+} 
